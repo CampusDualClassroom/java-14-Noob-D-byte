5cb74f67262da7b83cab96fe350e1f2dc2483493
davidrey <david_sardy@hotmail.com>
2024-11-16 12:08:19 +0100

    Exercise14 Resuelto

--- src/main/java/com/campusdual/classroom/Doctor.java
+++ src/main/java/com/campusdual/classroom/Doctor.java
@@ -2,10 +2,16 @@ package com.campusdual.classroom;
 
 public class Doctor extends Person {
 
-	protected String specialization;
+    protected String specialization;
 
-	public Doctor(String name, String surname, String specialization) {
-		super(name, surname);
-		this.specialization = specialization;
-	}
+    public Doctor(String name, String surname, String specialization) {
+        super(name, surname);
+        this.specialization = specialization;
+    }
+
+    @Override//Genero mediante o click dereito este metodo, e imprimo por pantalla a mensaxe para cada clase.
+    //neste caso specialization para Doctor
+    public void getDetails() {
+        System.out.println("Este doctor chámase " + name + " " + surname + " e dedicase a " + specialization);
+    }
 }
--- src/main/java/com/campusdual/classroom/Teacher.java
+++ src/main/java/com/campusdual/classroom/Teacher.java
@@ -2,10 +2,17 @@ package com.campusdual.classroom;
 
 public class Teacher extends Person {
 
-	protected String area;
+    protected String area;
 
-	public Teacher(String name, String surname, String area) {
-		super(name, surname);
-		this.area = area;
-	}
+    public Teacher(String name, String surname, String area) {
+        super(name, surname);
+        this.area = area;
+    }
+
+    @Override
+    public void getDetails() {//Genero mediante o click dereito este metodo, e imprimo por pantalla a mensaxe para cada clase.
+        //neste caso area para o Teacher
+        System.out.println("Esta persoa chámase " + name + " " + surname + " e dedicase a " + area);
+
+    }
 }
